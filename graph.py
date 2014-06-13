0d2ba14ceebfb816aed55f5ed4012267a444b55b
Shubham Bhushan <konsbn@outlook.com>
2014-06-13 17:34:44 +0530

    Finally the DFS for cluster isolation worked

--- graph.py
+++ graph.py
@@ -10,6 +10,7 @@ class graph(object):
 
 def adj_list(graph):
 	edges = graph.edges
+	nodes = graph.nodes
 	adj = {}
 	for i in edges:
 		new_key = i[0]
@@ -18,10 +19,35 @@ def adj_list(graph):
 			adj[new_key].append(add_key)
 		else:
 			adj[new_key] = [add_key]
-		# if new_key in adj.keys() == True:
-		# 	adj[new_key].append(add_key)
-		# if new_key in adj.keys() == False:
-		# 	adj[new_key] = [add_key]
 	return adj
 
-#def seen()
+def filter(graph):
+	adj = adj_list(graph)
+	nodes = graph.nodes
+	for i in nodes:
+		if i not in adj:
+			adj[i] = None
+	return adj
+
+
+
+def dfs_visit( adj, s, parent = {}):
+	
+	if parent == {}:
+		parent[s] = None
+	try:	
+		for v in adj[s]:
+			if v not in parent:
+					parent[v] = s
+					dfs_visit( adj, v, parent)
+	except TypeError:
+		pass
+	return parent
+
+def dfs(adj,v):
+	parent = {}
+	for s in v:
+		if s not in parent:
+			parent[s] = None
+			dfs_visit(adj, s, parent)
+	return parent
